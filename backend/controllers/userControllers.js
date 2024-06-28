b1ddd206a72923f330c2e1654d85c1acba92e6da
abhinandsj <abhinandsj28@gmail.com>
2024-06-29 01:23:50 +0530

    stash changes

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -3,54 +3,55 @@ const asyncHandler = require("express-async-handler");
 const generateToken = require("../utils/generateToken");
 
 const registerUser = asyncHandler(async (req, res) => {
-  const { name, email, password, pic } = req.body;
+     const { name, email, password, pic, isAdmin } = req.body;
 
-  const userExists = await User.findOne({ email });
+     const userExists = await User.findOne({ email });
 
-  if (userExists) {
-    res.status(400);
-    throw new Error("User Already Exists");
-  }
+     if (userExists) {
+          res.status(400);
+          throw new Error("User Already Exists");
+     }
 
-  const user = await User.create({
-    name,
-    email,
-    password,
-    pic,
-  });
+     const user = await User.create({
+          name,
+          email,
+          password,
+          pic,
+          isAdmin,
+     });
 
-  if (user) {
-    res.status(201).json({
-      _id: user._id,
-      name: user.name,
-      email: user.email,
-      isAdmin: user.isAdmin,
-      pic: user.pic,
-      token: generateToken(user._id),
-    });
-  } else {
-    res.status(400);
-    throw new Error("Error Occured! ");
-  }
+     if (user) {
+          res.status(201).json({
+               _id: user._id,
+               name: user.name,
+               email: user.email,
+               isAdmin: user.isAdmin,
+               pic: user.pic,
+               token: generateToken(user._id),
+          });
+     } else {
+          res.status(400);
+          throw new Error("Error Occured! ");
+     }
 });
 
 const authUser = asyncHandler(async (req, res) => {
-  const { email, password } = req.body;
+     const { email, password } = req.body;
 
-  const user = await User.findOne({ email });
+     const user = await User.findOne({ email });
 
-  if (user && (await user.matchPassword(password))) {
-    res.json({
-      _id: user._id,
-      name: user.name,
-      email: user.email,
-      isAdmin: user.isAdmin,
-      pic: user.pic,
-      token: generateToken(user._id),
-    });
-  } else {
-    res.status(400);
-    throw new Error("Invalid Email or Password");
-  }
+     if (user && (await user.matchPassword(password))) {
+          res.json({
+               _id: user._id,
+               name: user.name,
+               email: user.email,
+               isAdmin: user.isAdmin,
+               pic: user.pic,
+               token: generateToken(user._id),
+          });
+     } else {
+          res.status(400);
+          throw new Error("Invalid Email or Password");
+     }
 });
 module.exports = { registerUser, authUser };
