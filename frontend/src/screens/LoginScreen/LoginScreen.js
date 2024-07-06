4956f1d7a4572769a95a2e32dca7659de8477b21
abhinandsj <abhinandsj28@gmail.com>
2024-07-06 19:48:22 +0530

    loginpage ui change

--- frontend/src/screens/LoginScreen/LoginScreen.js
+++ frontend/src/screens/LoginScreen/LoginScreen.js
@@ -10,74 +10,84 @@ import Loading from "../../components/Loading";
 import ErrorMessage from "../../components/ErrorMessage";
 import { useDispatch, useSelector } from "react-redux";
 import { login } from "../../actions/userActions";
+import { FaUser, FaLock } from "react-icons/fa";
 
 const LoginScreen = ({ history }) => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+     const [email, setEmail] = useState("");
+     const [password, setPassword] = useState("");
 
-  const dispatch = useDispatch();
+     const dispatch = useDispatch();
 
-  const userLogin = useSelector((state) => state.userLogin);
-  const { loading, error, userInfo } = userLogin;
-  const navigate = useNavigate();
+     const userLogin = useSelector((state) => state.userLogin);
+     const { loading, error, userInfo } = userLogin;
+     const navigate = useNavigate();
 
-  useEffect(() => {
-    if (userInfo) {
-      navigate("/myhouses");
-    }
-  }, [navigate, userInfo]);
+     useEffect(() => {
+          if (userInfo) {
+               navigate("/myhouses");
+          }
+     }, [navigate, userInfo]);
 
-  const submitHandler = async (e) => {
-    e.preventDefault();
+     const submitHandler = async (e) => {
+          e.preventDefault();
 
-    dispatch(login(email, password));
-  };
+          dispatch(login(email, password));
+     };
 
-  return (
-    <MainScreen title="LOGIN">
-      <div className="loginContainer">
-        {error && <ErrorMessage variant="danger">{error}</ErrorMessage>}
-        {loading && <Loading />}
-        <Form onSubmit={submitHandler}>
-          <Form.Group controlId="formBasicEmail" className="my-3">
-            <Form.Label>Email Address</Form.Label>
-            <Form.Control
-              type="email"
-              value={email}
-              placeholder="Enter email"
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </Form.Group>
-          <Form.Group controlId="formBasicPassword" className="my-3">
-            <Form.Label>Password</Form.Label>
-            <Form.Control
-              type="password"
-              value={password}
-              placeholder="Enter password"
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </Form.Group>
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
-        </Form>
-        <Row className="py-3">
-          <Col>
-            New Customer?{" "}
-            <Link
-              to="/register"
-              style={{
-                textDecoration: "underline",
-                color: "blue",
-              }}
-            >
-              Register Here
-            </Link>
-          </Col>
-        </Row>
-      </div>
-    </MainScreen>
-  );
+     return (
+          <div className="loginContainer poppins-regular">
+               {error && <ErrorMessage variant="danger">{error}</ErrorMessage>}
+               {loading && <Loading />}
+               <div>
+                    <Form onSubmit={submitHandler} className="">
+                         <h1 className="poppins-bold">Login</h1>
+                         <Form.Group
+                              controlId="formBasicEmail"
+                              className="my-3"
+                         >
+                              <Form.Label>Email Address</Form.Label>
+                              <Form.Control
+                                   type="email"
+                                   value={email}
+                                   placeholder="Enter email"
+                                   onChange={(e) => setEmail(e.target.value)}
+                              />
+                              <FaUser />
+                         </Form.Group>
+                         <Form.Group
+                              controlId="formBasicPassword"
+                              className="my-3"
+                         >
+                              <Form.Label>Password</Form.Label>
+                              <Form.Control
+                                   type="password"
+                                   value={password}
+                                   placeholder="Enter password"
+                                   onChange={(e) => setPassword(e.target.value)}
+                              />
+                              <FaLock />
+                         </Form.Group>
+                         <Button variant="primary" type="submit">
+                              Submit
+                         </Button>
+                    </Form>
+                    <Row className="py-3">
+                         <Col>
+                              New Customer?{" "}
+                              <Link
+                                   to="/register"
+                                   style={{
+                                        textDecoration: "underline",
+                                        color: "blue",
+                                   }}
+                              >
+                                   Register Here
+                              </Link>
+                         </Col>
+                    </Row>
+               </div>
+          </div>
+     );
 };
 
 export default LoginScreen;
